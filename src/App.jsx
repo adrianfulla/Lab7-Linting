e47c59ad5aacbfd264fa1ca1d76a876200a16138
Adrian Fulladolsa <ful21592@uvg.edu.gt>
2023-05-10 10:52:29 -0600

    fork de Glitch-uvg-DuckDuck-main

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
 import './App.css'
+import Buscador from './components/Buscador/Buscador'
+import './components/info_blocks/main_infoBlocks'
+import MainInfoBlocks from './components/info_blocks/main_infoBlocks'
+import LastImage from './components/LastImage/LastImage'
+import Onboarding from './components/onboarding/Onboarding'
+import FAQ from './components/FAQ/FAQ'
+import DropDownMenu from "./components/DropDownMenu/DropDownMenu.jsx"
+import SocialMedia from "./components/SocialMedia/SocialMedia.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+
+    <div className="App">
+      <header className='App-header'>
+          <SocialMedia />
+        <DropDownMenu />
+      </header>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Buscador></Buscador>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+      <div className='fondo'>
+        <svg className="content-info__curve" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1438 134">
+          <path d="M1438 1442H0V31.001S438.105 0 719 .001c280.896 0 719 31 719 31V1442z" fill="currentColor"></path>
+        </svg>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <MainInfoBlocks className ='mainInfo'></MainInfoBlocks>
+      </div>
+      <div>
+        <Onboarding></Onboarding>
+      </div>
+      <div>
+      <LastImage></LastImage>
+      </div>
+      <div>
+      <FAQ></FAQ>
+      </div>
+    </div>
   )
 }
 
